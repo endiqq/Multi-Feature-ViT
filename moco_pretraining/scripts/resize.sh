99edec41965bed75c0023db88593b3962a2d61a2
endiqq <31194584+endiqq@users.noreply.github.com>
2022-07-31 22:19:30 -0400

    Delete moco_pretraining directory

--- moco_pretraining/scripts/resize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Convert all images in $1 to $2
-
-mkdir -p $2
-
-for filename in $1/*; do
-    # echo $filename
-    convert $filename -resize 500x500! $2/$(basename "$filename")
-done
